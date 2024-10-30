10c1b9f7e058f43f2296a7309fe66d6ec2532fd1
Jacob Rast <jrast@andrew.cmu.edu>
2024-10-30 16:31:41 -0400

    set hyperparameters to reasonable defaults

--- train_gpt2.cu
+++ train_gpt2.cu
@@ -1367,26 +1367,26 @@ int main(int argc, char *argv[]) {
     const char* val_data_pattern = "data/fineweb_val_*.bin";
     const char* tokenizer_path = "data/gpt2_tokenizer.bin";
     const char* hellaswag_path = "data/hellaswag_val.bin";
-    const char* load_filename = "gpt2_124M_bf16.bin"; // bf16 weights of the model
+    const char* load_filename;
     const char* lr_scheduler_type = "cosine";
-    const char* output_log_dir = NULL;
-    int checkpoint_every = 0; // write checkpoints every how many steps?
+    const char* output_log_dir = "log";
+    int checkpoint_every = 1000; // write checkpoints every how many steps?
     int checkpoints_keep = 0; // how long checkpoint history do we keep? (in units of checkpoints)
     int major_checkpoint_every = 0; // major checkpoints never get deleted when maintaining history
     int resume = 0; // resume the optimization, if one is found inside output_log_dir?
     int B = 4; // batch size
     int T = 1024; // sequence length max
-    int total_batch_size = -1; // will be calculated down below later, if not provided
+    int total_batch_size = 524288; // will be calculated down below later, if not provided
     float learning_rate = 3e-4f;
     int log_gpu_every = -1;
-    int warmup_iterations = 0;
-    float final_learning_rate_frac = 1.0f; // final fraction of learning rate, at end of training
-    float weight_decay = 0.0f;
+    int warmup_iterations = 700;
+    float final_learning_rate_frac = 0.0f; // final fraction of learning rate, at end of training
+    float weight_decay = 0.1f;
     float skip_update_lossz = 0.0f; // skip update if loss goes above this in zscore
     float skip_update_gradz = 0.0f; // skip update if grad_norm goes above this in zscore
-    int val_loss_every = 20; // every how many steps do we eval validation loss?
+    int val_loss_every = 250; // every how many steps do we eval validation loss?
     int val_max_steps = 20; // how many batches max do we eval for validation loss?
-    int sample_every = 20; // every how many steps to do inference?
+    int sample_every = 20000; // every how many steps to do inference?
     int genT = 64; // number of steps of inference we will do
     int overfit_single_batch = 0; // useful for debugging, 1 = only load a single data batch once
     int max_steps = -1;
@@ -1394,8 +1394,8 @@ int main(int argc, char *argv[]) {
     int use_master_weights = 1;
     int gelu_fusion = -1; // 0 = none, 1 = forward, 2 = forward+backward (-1 => per-GPU default)
     int recompute = 1; // recompute during backward setting, 0 = none, 1 = recompute gelu
-    int zero_stage = 0; // Zero Optimization Stage for Multi-GPU training
-    int hellaswag_eval = 0;
+    int zero_stage = 1; // Zero Optimization Stage for Multi-GPU training
+    int hellaswag_eval = 1;
     // multi-node settings
     int num_processes = 1;  // this should be set by the slurm environment
     int process_rank = 0;  // this should be set by the slurm environment
