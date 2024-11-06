c4f05f29feecb9edbe14dac99ec23f159cc8ae75
chrispyroberts <98184754+chrispyroberts@users.noreply.github.com>
2024-11-05 20:20:36 -0500

    Update upload_to_s3.py

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,11 +1,11 @@
 import os
-import sys
 import boto3
+import sys
 
 def upload_directory_to_s3(local_directory, s3_bucket, s3_directory):
     # Use the S3 resource
     s3 = boto3.resource('s3', region_name='us-east-1')
-
+    
     for root, dirs, files in os.walk(local_directory):
         for file in files:
             local_file_path = os.path.join(root, file)
@@ -13,7 +13,6 @@ def upload_directory_to_s3(local_directory, s3_bucket, s3_directory):
             # Create the relative path to the file in S3
             relative_path = os.path.relpath(local_file_path, local_directory)
             s3_file_path = os.path.join(s3_directory, relative_path)
-
             try:
                 # Upload the file to S3
                 s3.Bucket(s3_bucket).upload_file(local_file_path, s3_file_path)
@@ -21,12 +20,21 @@ def upload_directory_to_s3(local_directory, s3_bucket, s3_directory):
             except Exception as e:
                 print(f'Error uploading {local_file_path}: {e}')
 
-
 if __name__ == "__main__":
-    local_experiments_directory = 'experiments'  # Path to your local experiments directory
-    s3_directory_name = 'experiments'            # Folder in S3 where you want to upload
+    # Check that we have the required arguments
+    if len(sys.argv) < 5:
+        print("Usage: python script.py <s3_bucket_name>  <local_folder_path> <cluster_name> <run_name>")
+        sys.exit(1)
 
-    s3_bucket_name = sys.argv[0]
+    # Get parameters from command line arguments
+    s3_bucket_name = sys.argv[1]          # Your S3 bucket name
+    local_folder_path = sys.argv[2]       # Path to your local folder
+    cluster_name = sys.argv[3]            # Cluster name
+    run_name = sys.argv[4]                # Run name
 
-    upload_directory_to_s3(local_experiments_directory, s3_bucket_name, s3_directory_name)
+    # Define the S3 target directory path
+    s3_directory_name = f"experiments/{cluster_name}/{run_name}"
+
+    # Upload the directory to the specified location in S3
+    upload_directory_to_s3(local_folder_path, s3_bucket_name, s3_directory_name)
     print("Done uploading to S3 Bucket")
